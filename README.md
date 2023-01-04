98ad368855dec9142760a2e29c88c09394dadaba
George R <rowla101@gmail.com>
2023-01-04 15:12:37 +0900

    Added build docs

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nextra Garden
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit `localhost:3000`.
+
+## Deployment
+
+Run `pnpm build` to generate an optimized version of the application for production. The output is generated inside the `.next` folder.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/LuciferUchiha/nextra-garden',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/LuciferUchiha/nextra-garden',
+  footer: {
+    text: 'Nextra Garden',
+  },
+}
+
+export default config
