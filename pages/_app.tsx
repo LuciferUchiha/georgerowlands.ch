fcf4c3b4cee4452d214b12a9575b74a817914de6
George R <rowla101@gmail.com>
2023-02-02 13:49:09 +0900

    Added gitalk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type {ReactElement} from 'react'
 import type {AppProps} from 'next/app'
+import 'gitalk/dist/gitalk.css'
 
 import '../styles/globals.css'
 
 export default function Nextra({
-        Component,
-        pageProps
-    }: AppProps): ReactElement {
+                                   Component,
+                                   pageProps
+                               }: AppProps): ReactElement {
     return <Component {...pageProps} />
 }
