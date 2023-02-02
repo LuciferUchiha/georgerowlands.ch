b9d251c7b87206432b0ac2a23ceff2845a057ad5
George R <rowla101@gmail.com>
2023-02-02 15:33:10 +0900

    Formatted code

--- components/Callout/callout.tsx
+++ components/Callout/callout.tsx
@@ -1,5 +1,5 @@
-import type {ReactElement, ReactNode} from 'react'
-import cn from 'clsx'
+import type {ReactElement, ReactNode} from "react";
+import cn from "clsx";
 import {VscRocket, VscInfo, VscWarning, VscError, VscBeaker, VscSymbolEvent} from "react-icons/vsc";
 
 
@@ -9,31 +9,31 @@ const TypeToEmoji = {
     warning: <VscWarning className="w-[28px] h-[28px]"/>,
     error: <VscError className="w-[28px] h-[28px]"/>,
     example: <VscBeaker className="w-[28px] h-[28px]"/>,
-    todo: <VscSymbolEvent className="w-[28px] h-[28px]"/>,
-}
+    todo: <VscSymbolEvent className="w-[28px] h-[28px]"/>
+};
 
 type CalloutType = keyof typeof TypeToEmoji
 
 const classes: Record<CalloutType, string> = {
     default: cn(
-        'border-primary-200 bg-primary-50 text-primary-800 dark:border-primary-400/30 dark:bg-primary-400/20 dark:text-primary-300'
+        "border-primary-200 bg-primary-50 text-primary-800 dark:border-primary-400/30 dark:bg-primary-400/20 dark:text-primary-300"
     ),
     error: cn(
-        'border-red-300 bg-red-100 text-red-900 dark:border-red-200/30 dark:bg-red-900/30 dark:text-red-200'
+        "border-red-300 bg-red-100 text-red-900 dark:border-red-200/30 dark:bg-red-900/30 dark:text-red-200"
     ),
     info: cn(
-        'border-blue-300 bg-blue-100 text-blue-900 dark:border-blue-200/30 dark:bg-blue-900/30 dark:text-blue-200'
+        "border-blue-300 bg-blue-100 text-blue-900 dark:border-blue-200/30 dark:bg-blue-900/30 dark:text-blue-200"
     ),
     warning: cn(
-        'border-yellow-200 bg-yellow-50 text-yellow-900 dark:border-yellow-200/30 dark:bg-yellow-700/30 dark:text-yellow-200'
+        "border-yellow-200 bg-yellow-50 text-yellow-900 dark:border-yellow-200/30 dark:bg-yellow-700/30 dark:text-yellow-200"
     ),
     example: cn(
-        'border-primary-200 bg-primary-50 text-primary-800 dark:border-primary-400/30 dark:bg-primary-400/20 dark:text-primary-300'
+        "border-primary-200 bg-primary-50 text-primary-800 dark:border-primary-400/30 dark:bg-primary-400/20 dark:text-primary-300"
     ),
     todo: cn(
-        'border-yellow-100 bg-yellow-50 text-yellow-900 dark:border-yellow-200/30 dark:bg-yellow-700/30 dark:text-yellow-200'
-    ),
-}
+        "border-yellow-100 bg-yellow-50 text-yellow-900 dark:border-yellow-200/30 dark:bg-yellow-700/30 dark:text-yellow-200"
+    )
+};
 
 type CalloutProps = {
     type?: CalloutType
@@ -42,27 +42,27 @@ type CalloutProps = {
 }
 
 export default function Callout({
-                                    children,
-                                    type = 'default',
-                                    emoji = TypeToEmoji[type]
-                                }: CalloutProps): ReactElement {
+    children,
+    type = "default",
+    emoji = TypeToEmoji[type]
+}: CalloutProps): ReactElement {
     return (
         <div
             className={cn(
-                'nextra-callout mt-6 flex rounded-lg border py-2 ltr:pr-4 rtl:pl-4',
-                'contrast-more:border-current contrast-more:dark:border-current',
+                "nextra-callout mt-6 flex rounded-lg border py-2 ltr:pr-4 rtl:pl-4",
+                "contrast-more:border-current contrast-more:dark:border-current",
                 classes[type]
             )}
         >
             <div
                 className="select-none text-xl font-bold capitalize ltr:pl-3 ltr:pr-2 rtl:pr-3 rtl:pl-2"
                 style={{
-                    fontFamily: '"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+                    fontFamily: "\"Apple Color Emoji\", \"Segoe UI Emoji\", \"Segoe UI Symbol\""
                 }}
             >
                 {emoji}
             </div>
             <div className="nx-w-full nx-min-w-0 nx-leading-7">{children}</div>
         </div>
-    )
+    );
 }
