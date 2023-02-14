3b38bae910aa8b6df511e3cda8be6aa98fa4d974
LuciferUchiha <rowla101@gmail.com>
2023-02-14 10:27:38 +0100

    Ported over more conent and started using frontmatter

--- components/progressBar/progressBar.tsx
+++ components/progressBar/progressBar.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function ProgressBar() {
     return (
-            <div className="h-6 rounded-2xl bg-primary-500 0 animate-progress"/>
+            <div className="h-6 rounded-2xl bg-primary-500 dark:bg-primary-700 0 animate-progress"/>
     );
 }
