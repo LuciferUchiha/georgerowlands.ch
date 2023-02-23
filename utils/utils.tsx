86328019a87e0cce7c6899351de110079568324a
LuciferUchiha <rowla101@gmail.com>
2023-02-23 16:39:04 +0100

    Cleaned up linear algebra and added title to callouts

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,1 +1,3 @@
 export const randRange = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
