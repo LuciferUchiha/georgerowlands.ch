c57d97efd508d9fcc253e50be088028d51577dec
George R <rowla101@gmail.com>
2023-07-20 13:51:13 +0200

    Added default theme and started writing

--- components/Graph/Graph.tsx
+++ components/Graph/Graph.tsx
@@ -1,7 +1,9 @@
 import {useState, useEffect} from "react";
 import VisGraph from "react-graph-vis";
 
-export default function Graph({graph, options, events}) {
+import 'vis-network/styles/vis-network.css';
+
+export default function Graph({graph, options, events, directed = false}) {
     const [showChild, setShowChild] = useState(false);
     useEffect(() => {
         setShowChild(true);
@@ -14,22 +16,31 @@ export default function Graph({graph, options, events}) {
     if (typeof window === "undefined") {
         return <></>;
     } else {
+
         const defaultOptions = {
             edges: {
-                color: "#000000"
+                color: "#7300E6",
+            },
+            nodes: {
+                color: "#7300E6",
+                font: "14px Arial #FFFFFF",
+                shape: "circle",
             },
             height: "500px"
         };
         // merge the default options with incoming options
         options = {...defaultOptions, ...options};
 
+        if (!directed) {
+            options = {...options, ...{edges: {...options.edges, ...{arrows: {to: false, from: false}}}}}
+        }
         return (
             <div className="border-2 border-primary-600 rounded-md dark:border-primary-700">
-            <VisGraph
-                graph={graph}
-                options={options}
-                events={events}
-            />
+                <VisGraph
+                    graph={graph}
+                    options={options}
+                    events={events}
+                />
             </div>
         );
     }
