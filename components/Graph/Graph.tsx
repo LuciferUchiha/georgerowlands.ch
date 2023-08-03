26f218a3e829484fe76cb5becd6e24536e14d4f3
George R <rowla101@gmail.com>
2023-08-03 15:20:11 +0200

    Finished connectivity

--- components/Graph/Graph.tsx
+++ components/Graph/Graph.tsx
@@ -3,7 +3,7 @@ import VisGraph from "react-graph-vis";
 
 import 'vis-network/styles/vis-network.css';
 
-export default function Graph({graph, options, events, directed = false}) {
+export default function Graph({graph, options, events, directed = false, nodeSize = 25, edgeWidth = 2}) {
     const [showChild, setShowChild] = useState(false);
     useEffect(() => {
         setShowChild(true);
@@ -13,12 +13,23 @@ export default function Graph({graph, options, events, directed = false}) {
         return null;
     }
 
+    function formatSingleLetterNodes(nodes: any) {
+        return nodes.map((node: any) => {
+            if (node.label.length === 1) {
+                return {...node, ...{label: " " + node.label + " "}};
+            } else {
+                return node;
+            }
+        });
+    }
+
     if (typeof window === "undefined") {
         return <></>;
     } else {
 
         const defaultOptions = {
             edges: {
+                width: edgeWidth,
                 color: "#7300E6",
                 font: {
                     color: "#FFFFFF",
@@ -28,13 +39,26 @@ export default function Graph({graph, options, events, directed = false}) {
             },
             nodes: {
                 color: "#7300E6",
-                font: "14px Arial #FFFFFF",
+                font: {
+                    color: "#FFFFFF",
+                },
+                scaling: {
+                    label: {
+                        enabled: true,
+                        min: 12,
+                        max: 30
+                    },
+                    min: 1,
+                    max: 100
+                },
                 shape: "circle",
             },
             height: "500px"
         };
         // merge the default options with incoming options
         options = {...defaultOptions, ...options};
+
+        graph.nodes = formatSingleLetterNodes(graph.nodes);
 
         if (!directed) {
             options = {...options, ...{edges: {...options.edges, ...{arrows: {to: false, from: false}}}}}
