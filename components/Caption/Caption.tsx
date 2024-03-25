7fe45d36a87d8365886377a5f88334dfd7b963cb
Lucifer Uchiha <rowla101@gmail.com>
2024-03-25 11:14:02 +0100

    removed everything

--- components/Caption/Caption.tsx
+++
@@ -1,8 +0,0 @@
-export default function Caption({caption = "", children}) {
-    return (
-        <figure className="mt-8 flex flex-col items-center">
-            {children}
-            <figcaption className="mt-2 italic">{caption}</figcaption>
-        </figure>
-    );
-};
--- pages/digitalGarden/mathematics/inGerman/integralrechnung/10-mehrfrachintegrale.md
+++
@@ -1,3 +0,0 @@
-# Mehrfrachintegrale
-
-Ein gutes Video zu wie man das macht findest du [hier](https://www.youtube.com/watch?v=n8NDyxOMVEw)
