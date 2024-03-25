83ccb69727113a1cc1ff97db3b047e5fc4ed7c89
Lucifer Uchiha <rowla101@gmail.com>
2024-03-25 14:34:41 +0100

    added style to name

--- components/Home/NameText.tsx
+++ components/Home/NameText.tsx
@@ -5,12 +5,14 @@ const NameText = () => {
     return (
         <div>
             <ReactTyped 
-                strings={["Hello there, "]}
+                strings={["Hello there,"]}
+                className="text-3xl"
                 typeSpeed={40}
                 showCursor={false}
             />
             <ReactTyped
                 strings={["my name is"]}
+                className="text-3xl ml-2"
                 typeSpeed={40}
                 startDelay={2000}
                 showCursor={false}
@@ -20,6 +22,7 @@ const NameText = () => {
 
             <ReactTyped
                 strings={["George Rowlands"]}
+                className="font-bold text-primary-500 dark:text-primary-700 text-6xl"
                 typeSpeed={40}
                 startDelay={3000}
                 showCursor={false}
