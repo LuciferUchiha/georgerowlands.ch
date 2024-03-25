5321a7cce9b6eb0bf95f9e89f7867932e92f673b
Lucifer Uchiha <rowla101@gmail.com>
2024-03-25 11:14:02 +0100

    removed everything

--- components/ProgressBar/ProgressBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ProgressBar() {
-    return (
-            <div className="h-6 rounded-2xl bg-primary-500 dark:bg-primary-700 0 animate-progress"/>
-    );
-}
