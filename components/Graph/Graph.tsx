b0d533139e45cd485cc4e5162d9298fd18702842
George R <rowla101@gmail.com>
2024-04-03 13:32:26 +0200

    graph links

--- components/Graph/Graph.tsx
+++ components/Graph/Graph.tsx
@@ -2,6 +2,8 @@ import { useTheme } from "nextra-theme-docs";
 import dynamic from "next/dynamic";
 import { useWindowDimensions } from "~/utils/windowDimensions";
 import Data from "~/data/graph.json";
+import SpriteText from "three-spritetext";
+import { useRef } from "react";
 
 const ForceGraph3D = dynamic(() => import("react-force-graph-3d"), {
   ssr: false,
@@ -11,6 +13,8 @@ const Graph = () => {
   const { theme, systemTheme } = useTheme();
   const currentTheme = theme !== "system" ? theme : systemTheme;
   const background = currentTheme === "dark" ? "#111" : "#fff";
+  const textColor = currentTheme === "dark" ? "#fff" : "#000";
+  const linkOpacity = currentTheme === "dark" ? 0.2 : 1;
 
   const windowDimensions = useWindowDimensions();
 
@@ -24,6 +28,18 @@ const Graph = () => {
       width={800}
       height={800}
       backgroundColor={background}
+      nodeThreeObject={node => {
+        const sprite = new SpriteText(node.id);
+        sprite.color = textColor;
+        sprite.textHeight = 8;
+        return sprite;
+      }}
+      nodeLabel={() => ''}
+      linkOpacity={linkOpacity}
+      onNodeHover={node => {
+        // TODO go to page on click
+        document.body.style.cursor = node ? "pointer" : null;
+      }}
     />
   );
 };
