a2db8ce09ee43f6ebb30be27f324f80023b9dbce
George R <rowla101@gmail.com>
2024-04-04 13:35:17 +0200

    some simple CS stuff

--- graph-builder/main.py
+++ graph-builder/main.py
@@ -24,12 +24,6 @@ if __name__ == "__main__":
                             nodes.append({"id": title, "name": title})
                         # TODO do more then just the titles and make links
 
-
-    # TODO for now just make links between all nodes
-    for i in range(len(nodes)):
-        for j in range(i + 1, len(nodes)):
-            links.append({"source": nodes[i]["id"], "target": nodes[j]["id"]})
-
     # save the nodes and links to a json file
     print("Exporting graph.json")
     with open("../data/graph.json", "w") as f:
