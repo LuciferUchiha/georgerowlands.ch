9a90c468f440472425d7468bab5edf18aee1484e
Lucifer Uchiha <rowla101@gmail.com>
2024-04-08 16:33:45 +0200

    some relations and sequences

--- graph-builder/main.py
+++ graph-builder/main.py
@@ -10,23 +10,44 @@ if __name__ == "__main__":
     nodes = []
     links = []
 
-    # walk through the entire digital garden
+    # walk through the entire digital garden to find all the nodes
     for root, dirs, files in os.walk(pages_path):
         for file in files:
             if file.endswith(".mdx"):
-                with open(os.path.join(root, file), "r") as f:
-                    print(f"Reading {os.path.join(root, file)}")
+                with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                    print(f"Node for {os.path.join(root, file)}")
                     content = f.read()
+                    # relative unix style path, no backslashes
+                    url = "/" + os.path.relpath(os.path.join(root, file), pages_path).replace("\\", "/")
+                    # remove the extension
+                    url = url.replace(".mdx", "")
                     for line in content.split("\n"):
                         # if it is a title make a node
                         if line.startswith("# "):
                             title = line[2:]
-                            nodes.append({"id": title, "name": title})
-                        # TODO do more then just the titles and make links
+                            nodes.append({"id": url, "name": title})
+
+    # walk through the entire digital garden to find all the links
+    for root, dirs, files in os.walk(pages_path):
+        for file in files:
+            if file.endswith(".mdx"):
+                with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                    print(f"Link for {os.path.join(root, file)}")
+                    content = f.read()
+                    # relative unix style path, no backslashes
+                    url = "/" + os.path.relpath(os.path.join(root, file), pages_path).replace("\\", "/")
+                    # remove the extension
+                    url = url.replace(".mdx", "")
+                    for node in nodes:
+                        if f"({node['id']})" in content:
+                            links.append({"source": url, "target": node["id"]})
+
+    print(f"Nodes: {len(nodes)}")
+    print(f"Links: {len(links)}")
 
     # save the nodes and links to a json file
     print("Exporting graph.json")
     with open("../data/graph.json", "w") as f:
-        json.dump({"nodes": nodes, "links": links}, f)
+        json.dump({"nodes": nodes, "links": links}, f, sort_keys=True, indent=4, separators=(',', ': '))
 
     print("Completed graph-builder")
