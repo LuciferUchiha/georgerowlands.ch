c8174243466a313bc12234ea80937330447d5b4a
Lucifer Uchiha <rowla101@gmail.com>
2024-07-27 17:11:20 +0200

    fixed styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,75 @@
 import type { Config } from "tailwindcss"
+const colors = require("tailwindcss/colors");
+
+// basically all of this is copied from the docs-theme which allows me to have the same settings as the nextra docs theme
+// without having to use the nx prefix (mainly wanted this for the primary color amonst other things).
+const makePrimaryColor =
+    l =>
+        ({opacityValue}) => {
+            if (opacityValue === undefined) {
+                return `hsl(var(--nextra-primary-hue) 100% ${l}%)`;
+            }
+            return `hsl(var(--nextra-primary-hue) 100% ${l}% / ${opacityValue})`;
+        };
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './theme.config.tsx'
   ],
   prefix: "",
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px"
+    },
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem"
+    },
+    letterSpacing: {
+      tight: "-0.015em"
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      gray: colors.gray,
+      slate: colors.slate,
+      neutral: colors.neutral,
+      red: colors.red,
+      orange: colors.orange,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      green: colors.green,
+      primary: {
+        50: makePrimaryColor(97),
+        100: makePrimaryColor(94),
+        200: makePrimaryColor(86),
+        300: makePrimaryColor(77),
+        400: makePrimaryColor(66),
+        500: makePrimaryColor(50),
+        600: makePrimaryColor(45),
+        700: makePrimaryColor(39),
+        750: makePrimaryColor(35),
+        800: makePrimaryColor(32),
+        900: makePrimaryColor(24)
+      }
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -18,6 +78,9 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        dark: "#111"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -27,10 +90,29 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        progress: {
+          "0%": {width: "0%"},
+          "100%": {width: "100%"}
+        },
+        noise: {
+          "0%": {transform: "translate(0,0)"},
+          "10%": {transform: "translate(-5%,-5%)"},
+          "20%": {transform: "translate(-10%,5%)"},
+          "30%": {transform: "translate(5%,-10%)"},
+          "40%": {transform: "translate(-5%,15%)"},
+          "50%": {transform: "translate(-10%,5%)"},
+          "60%": {transform: "translate(15%,0)"},
+          "70%": {transform: "translate(0,10%)"},
+          "80%": {transform: "translate(-15%,0)"},
+          "90%": {transform: "translate(10%,5%)"},
+          "100%": {transform: "translate(5%,0)"}
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        progress: "progress 5s infinite",
+        noise: "noise .2s infinite;"
       },
     },
   },
