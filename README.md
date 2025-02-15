337fcd1c7061d89accdaa58f8ccfe680cbb4c32d
George R <rowla101@gmail.com>
2025-02-15 15:46:54 +0100

    init

--- README.md
+++
@@ -1,5 +0,0 @@
-# georgerowlands.ch
-
-## Installation
-
-## Usage
--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,10 @@
-import nextra from 'nextra';
-import simplePlantUML from '@akebifiky/remark-simple-plantuml';
-
+import nextra from 'nextra'
+ 
 const withNextra = nextra({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.tsx',
-    staticImage: true,
-    latex: true,
-    flexsearch: {
-        codeblocks: false
-    },
-    defaultShowCopyCode: true,
-    mdxOptions: {
-        remarkPlugins: [
-            [
-                simplePlantUML,
-                {baseUrl: "https://www.plantuml.com/plantuml/svg"}
-            ]
-        ]
-    }
-});
-
+  // ... Other Nextra config options
+})
+ 
+// You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
-    images: {
-        formats: [ 'image/webp' ],
-        // it can 0 also for disabling
-        minimumCacheTTL: 60
-    },
-});
+  // ... Other Next.js config options
+})
