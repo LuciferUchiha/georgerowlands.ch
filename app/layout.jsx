3ca517fd1553940c4617cb6612bb21f2036632f3
George R <rowla101@gmail.com>
2025-02-17 10:22:17 +0100

    getting there

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,65 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import './globals.css'
+import BuyMeACoffee from '../public/buymeacoffee.svg'
+import Image from 'next/image'
  
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
  
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
   <Navbar
     logo={<b>Nextra</b>}
     // ... Your additional navbar options
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
- 
+
+const footer = (
+<Footer>
+  <div className="flex w-full flex-row items-center justify-between">
+    <ul>
+      <li>
+        <p className="font-bold">Socials</p>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/georgerowlands/"
+          className="text-sm"
+        >
+          Linkedin↗
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/LuciferUchiha" className="text-sm">
+          GitHub↗
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://stackoverflow.com/users/10994912/george-r"
+          className="text-sm"
+        >
+          StackOverflow↗
+        </a>
+      </li>
+    </ul>
+    <a href="https://www.buymeacoffee.com/georgerowlands" target="_blank">
+      <Image
+        src={BuyMeACoffee}
+        alt="Buy Me A Coffee"
+        width={250}
+      />
+    </a>
+  </div>
+</Footer>
+)
+
 export default async function RootLayout({ children }) {
+
   return (
     <html
       // Not required, but good for SEO
@@ -35,12 +76,22 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          sidebar={{ autoCollapse: true }}
+          docsRepositoryBase="https://github.com/LuciferUchiha/georgerowlands.ch/tree/main/"
           footer={footer}
           // ... Your additional layout options
+          nextThemes = {{
+            defaultTheme: "dark",
+          }}
+          editLink= "Edit this page on GitHub ✍🏻"
+          feedback={{
+            content: "Questions or Suggestions? Give me feedback"
+          }}
+          toc={{
+            title: "Table of Contents",
+          }}
         >
           {children}
         </Layout>
