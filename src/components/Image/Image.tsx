e336588839e14f5502179d5f120d83b6b90db616
George R <rowla101@gmail.com>
2025-02-17 17:13:49 +0100

    dont use latex in headers

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -3,17 +3,22 @@ import Caption from "@components/Caption/Caption";
 
 export default function Image({
   src,
-  alt,
+  alt = "",
   width = 800,
   height = 0,
   caption = "",
 }: {
   src: string;
-  alt: string;
+  alt?: string;
   width?: number;
   height?: number;
   caption?: string;
 }) {
+  // if a caption is defined but not an alt text, use the caption as alt text
+  if (!alt && caption) {
+    alt = caption;
+  }
+
   return (
     <Caption caption={caption}>
       <NextImage src={src} alt={alt} width={width} height={height} />
