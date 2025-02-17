06dcdfda413b14298f0bfcd8e88929f59def5660
George R <rowla101@gmail.com>
2025-02-17 19:09:05 +0100

    Added giscus

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -1,12 +1,13 @@
 import { generateStaticParamsFor, importPage } from 'nextra/pages'
 import { useMDXComponents } from '../../src/mdx-components'
- 
+import GiscusComments from '@components/GiscusComments/GiscusComments'
+
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
- 
+
 export async function generateMetadata(props) {
   const params = await props.params
   const { metadata } = await importPage(params.mdxPath)
-  
+
   // when we are at the root params is an empty object
   const title = Object.keys(params).length === 0 ? 'georgerowlands.ch' : `${metadata.title} - GR`;
 
@@ -15,9 +16,9 @@ export async function generateMetadata(props) {
     title,
   }
 }
- 
+
 const Wrapper = useMDXComponents().wrapper
- 
+
 export default async function Page(props) {
   const params = await props.params
   const result = await importPage(params.mdxPath)
@@ -25,6 +26,9 @@ export default async function Page(props) {
   return (
     <Wrapper toc={toc} metadata={metadata}>
       <MDXContent {...props} params={params} />
+      <div className="mt-8">
+        <GiscusComments />
+      </div>
     </Wrapper>
   )
 }
