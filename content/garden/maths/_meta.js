e74828b505167a7233c2804b3fdf77ba0f16b123
George R <rowla101@gmail.com>
2025-02-21 18:54:47 +0100

    Added linalg

--- content/garden/maths/_meta.js
+++ content/garden/maths/_meta.js
@@ -2,6 +2,7 @@ export default {
     discrete: "Discrete Maths",
     calculus: "Calculus",
     probabilityStatistics: "Probability and Statistics",
+    linearAlgebra: "Linear Algebra",
     complexNumbers: "Complex Numbers",
     commonFunctions: "Common Maths Functions",
     latexGuidelines: "",
