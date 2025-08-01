446598d64dbdc316400e6375cb7e2d600ca9b0ec
George R. <rowla101@gmail.com>
2025-08-01 16:03:41 +0200

    Add sequences of functions section and enhance convergent functions documentation

--- content/garden/maths/calculus/_meta.js
+++ content/garden/maths/calculus/_meta.js
@@ -7,6 +7,7 @@ export default {
     convergentSeries: "",
     realFunctions: "",
     continuousFunctions: "",
+    sequencesOfFunctions: "",
     convergentFunctions: "",
     limitRules: "",
     differentiableFunctions: "",
