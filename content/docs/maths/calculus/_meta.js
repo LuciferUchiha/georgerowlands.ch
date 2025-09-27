bfb6dcd085491df0be5d205232ec968ba9825470
George R. <rowla101@gmail.com>
2025-09-27 11:43:27 +0200

    added old probability and calculus

---
+++ content/docs/maths/calculus/_meta.js
@@ -0,0 +1,19 @@
+export default {
+    sequences: "",
+    sumOperator: "",
+    productOperator: "",
+    convergentSequences: "",
+    series: "",
+    convergentSeries: "",
+    continuousFunctions: "",
+    convergentFunctions: "",
+    limitRules: "",
+    differentiableFunctions: "",
+    riemannIntegrals: "",
+    numericalIntegration: "",
+    power: "",
+    taylor: "",
+    fourier: "",
+    functionAnalysis: "",
+}
+  
---
+++ content/docs/maths/probabilityStatistics/_meta.js
@@ -0,0 +1,16 @@
+export default {
+  combinatorics: "",
+  probabilitySpaces: "",
+  conditionalProbabilityIndependence: "",
+  randomVariables: "",
+  expectationVarianceCovariance: "",
+  jointDistributions: "",
+  largeSamples: "",
+  estimators: "",
+  confidenceIntervals: "",
+  hypothesisTests: "",
+  algorithms: "",
+  markovChains: "",
+  queueingTheory: "",
+  simulating: ""
+}
